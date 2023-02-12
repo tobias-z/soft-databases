e3ba8991512813c2a7ee3693d044506e42fbedea
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-02-12 14:06:37 +0100

    fix: add correct database init script

--- sql/retail-sales/create-database.sql
+++ sql/retail-sales/create-database.sql
@@ -1,12 +1,27 @@
--- Create a new database called 'RetailSales'
--- Connect to the 'master' database to run this snippet
-USE master
-GO
--- Create the new database if it does not exist already
-IF NOT EXISTS (
-    SELECT [name]
-        FROM sys.databases
-        WHERE [name] = N'RetailSales'
-)
-CREATE DATABASE RetailSales
-GO
+CREATE TABLE [dbo].[Customers] (
+    [CustomerID] BIGINT       IDENTITY (1, 1) NOT NULL,
+    [FirstName]  VARCHAR (30) NOT NULL,
+    [LastName]   VARCHAR (30) NOT NULL,
+    [Email]      VARCHAR (50) NULL,
+    [Phone]      INT          NULL,
+    CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED ([CustomerID] ASC)
+);
+
+CREATE TABLE [dbo].[Products] (
+    [ProductID]   BIGINT       IDENTITY (1, 1) NOT NULL,
+    [ProductName] VARCHAR (50) NOT NULL,
+    [Description] TEXT         NULL,
+    [Price]       MONEY        NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY CLUSTERED ([ProductID] ASC)
+);
+
+CREATE TABLE [dbo].[Sales] (
+    [SaleID]     BIGINT   IDENTITY (1, 1) NOT NULL,
+    [SaleDate]   DATETIME NOT NULL,
+    [CustomerID] BIGINT   NOT NULL,
+    [ProductID]  BIGINT   NOT NULL,
+    [Quantity]   INT      NOT NULL,
+    CONSTRAINT [PK_Sales] PRIMARY KEY CLUSTERED ([SaleID] ASC),
+    CONSTRAINT [FK_Sales_Customers] FOREIGN KEY ([CustomerID]) REFERENCES [dbo].[Customers] ([CustomerID]),
+    CONSTRAINT [FK_Sales_Products] FOREIGN KEY ([ProductID]) REFERENCES [dbo].[Products] ([ProductID])
+);
