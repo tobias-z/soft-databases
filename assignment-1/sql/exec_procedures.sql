486e8847a3755ed8126ac1579819a8f956b76060
Tobias Zimmermann <tobias.zimmer007@gmail.com>
2023-02-22 21:19:33 +0100

    feat: add a magazine and use the select sp

--- assignment-1/sql/exec_procedures.sql
+++ assignment-1/sql/exec_procedures.sql
@@ -32,3 +32,27 @@ EXEC sp_upsert_item
     @ISBN = '0-5599-9135-5',
     @author_id = 1,
     @out_item_id = @out_item_id OUTPUT
+
+EXEC sp_upsert_item
+    @availability = 1,
+    @publisher_id = 1,
+    @title = 'How to build',
+    @number_of_pages = 20,
+    @item_type = 'magazine',
+    @ISBN = '0-5599-9135-6',
+    @author_id = 1,
+    @out_item_id = @out_item_id OUTPUT
+
+EXEC sp_ensure_genre_link
+    @genre_name = 'Building',
+    @published_item_id = @out_item_id;
+
+EXEC sp_ensure_genre_link
+    @genre_name = 'Work',
+    @published_item_id = @out_item_id;
+
+EXEC sp_find_items
+
+EXEC sp_find_items @item_type = 'book'
+
+EXEC sp_find_items @item_type = 'magazine';
